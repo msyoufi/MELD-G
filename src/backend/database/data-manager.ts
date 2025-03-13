bd2bbff5cc4a04eb0d7b042a1824544aac7ac83d
msyoufi <msyoufi.dev@gmail.com>
2025-03-13 20:35:40 +0100

    Refactored getCaseData()

--- src/backend/database/data-manager.ts
+++ src/backend/database/data-manager.ts
@@ -30,7 +30,7 @@ export function searchPatients_advanced(e: any, filters: AdvancedSearchForm): Pa
       results = DB.prepare(`
         SELECT * FROM patients WHERE id = (
           SELECT patient_id FROM MRIs WHERE study_id = @studyId
-          )
+        )
         ORDER BY surename
       `).all({ studyId }) as PatientInfos[];
 
@@ -38,9 +38,9 @@ export function searchPatients_advanced(e: any, filters: AdvancedSearchForm): Pa
       results = DB.prepare(`
         SELECT * FROM patients WHERE id IN (
           SELECT patient_id FROM MRIs WHERE id IN (
-              SELECT mri_id FROM annotations WHERE entity_code = @entityCode
-              )
+            SELECT mri_id FROM annotations WHERE entity_code = @entityCode
           )
+        )
         ORDER BY surename
       `).all({ entityCode }) as PatientInfos[];
     }
@@ -75,34 +75,59 @@ export function createCase(patient: Omit<PatientInfos, 'id'>): PatientInfos | nu
 
 export function getCase(patient: PatientInfos): MELDCase {
   try {
-    const MRIs: Map<string, MRI> = new Map();
-    const annotations: Map<string, Annotation> = new Map();
-
-    const mrisStatement = DB.prepare(`
-      SELECT MRIs.*, annotations.* FROM MRIs
-      LEFT JOIN annotations ON annotations.mri_id = MRIs.id
-      WHERE patient_id = @id
-    `);
-
-    const rows = mrisStatement.all({ id: patient.id }) as (MRI & Annotation)[]
-
-    for (const row of rows) {
-      const { id, patient_id, study_id, ...annotation } = row;
-      const mri_id = id.toString();
-
-      if (!MRIs.has(mri_id))
-        MRIs.set(mri_id, { id, patient_id, study_id });
-
-      // mri_id is only defined if MRI has annotations !!
-      if (annotation.mri_id)
-        annotations.set(annotation.ann_id.toString(), annotation);
-    }
-
-    const meld = DB.prepare(`
-      SELECT * FROM meld WHERE patient_id = @id
-    `).get({ id: patient.id }) as MELD;
+    const mrisWithAnnotations = getCaseMRIs(patient.id);
+    const { MRIs, annotations } = separateMIRs_Annotations(mrisWithAnnotations);
+    const meld = getCaseMeldData(patient.id);
 
     return { patient, MRIs, annotations, meld };
+
+  } catch (err: unknown) {
+    console.log(err);
+    throw err;
+  }
+}
+
+function getCaseMRIs(patientId: number | bigint): (MRI & Annotation)[] {
+  try {
+    return DB.prepare(`
+      SELECT MRIs.*, annotations.* FROM MRIs
+      LEFT JOIN annotations ON annotations.mri_id = MRIs.id
+      WHERE patient_id = @patientId
+    `).all({ patientId }) as (MRI & Annotation)[]
+
+  } catch (err: unknown) {
+    console.log(err);
+    throw err;
+  }
+}
+
+function separateMIRs_Annotations(mrisWithAnnotations: (MRI & Annotation)[]): {
+  MRIs: Map<string, MRI>,
+  annotations: Map<string, Annotation>
+} {
+  const MRIs: Map<string, MRI> = new Map();
+  const annotations: Map<string, Annotation> = new Map();
+
+  for (const mriWihtAnn of mrisWithAnnotations) {
+    const { id, patient_id, study_id, ...annotation } = mriWihtAnn;
+    const mri_id = id.toString();
+
+    if (!MRIs.has(mri_id))
+      MRIs.set(mri_id, { id, patient_id, study_id });
+
+    // mri_id is only defined if MRI has annotations !!
+    if (annotation.mri_id)
+      annotations.set(annotation.ann_id.toString(), annotation);
+  }
+
+  return { MRIs, annotations };
+}
+
+function getCaseMeldData(patientId: number | bigint): MELD {
+  try {
+    return DB.prepare(`
+      SELECT * FROM meld WHERE patient_id = @patientId
+    `).get({ patientId }) as MELD;
 
   } catch (err: unknown) {
     console.log(err);
